8263dd8c9c32e65d54d1b6989197b60013a56689
kev-torrico <kevtorricov@gmail.com>
2024-07-09 02:47:10 +0000

    Add the excuse generator using javaScript

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function aleatorio(lista) {
+    let numeroAleatorio = Math.floor(Math.random() * lista.length);
+    return lista[numeroAleatorio] + " ";
+  }
+
+  let who = [
+    "The dog",
+    "My grandma",
+    "The mailman",
+    "My bird",
+    "Messi",
+    "Ronaldo"
+  ];
+  let action = ["ate", "peed", "crushed", "broke", "jumped", "stole"];
+  let what = ["my homework", "my phone", "the car", "my house", "my tv"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying",
+    "when I was in the school",
+    "when I took a shower"
+  ];
+
+  console.log(
+    aleatorio(who) + aleatorio(action) + aleatorio(what) + aleatorio(when)
+  );
+  // who + acion + what + when
 };
